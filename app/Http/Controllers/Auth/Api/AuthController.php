ceef2b3ae4a04010c6217d6306b84656ac7cd85b
João Vunge <jvunge@africellao.local>
2024-09-01 15:31:36 +0100

    Update 0109202403

--- app/Http/Controllers/Auth/Api/AuthController.php
+++ app/Http/Controllers/Auth/Api/AuthController.php
@@ -78,7 +78,7 @@ class AuthController extends Controller
             $pass = Hash::make($request->password);
             $request->password = $pass;
 
-            if($create = $post->create($request->all())){
+            if($create = $this->post->create($request->all())){
                 return response()->json([
                     'data' => $create,
                     'message' => 'Usuario criado com sucesso!'
